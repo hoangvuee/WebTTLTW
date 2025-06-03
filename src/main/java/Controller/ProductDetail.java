9361015ce1383c497d9b4e94a2be95ec034d6cf8
Viet <kiritoak01@gmail.com>
2025-06-03 22:09:49 +0700

    update cho lớp ProductDetail

--- src/main/java/Controller/ProductDetail.java
+++ src/main/java/Controller/ProductDetail.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Dao.CommentDao;
+import Models.Comment.Comments;
 import Models.Products.Products;
 import Services.ServiceProduct;
 import jakarta.servlet.ServletException;
@@ -17,22 +19,28 @@ import java.sql.SQLException;
 )
 public class ProductDetail extends HttpServlet {
     Products pro = new Products();
-   ServiceProduct serviceProduct = new ServiceProduct();
+    ServiceProduct serviceProduct = new ServiceProduct();
+    CommentDao commentDao = new CommentDao();
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
- String id  = req.getParameter("id");
+        String id = req.getParameter("id");
         try {
             pro = serviceProduct.getProductDetail(id);
-
+            
+            // Load comments for this product
+            if (id != null && !id.isEmpty()) {
+                int productId = Integer.parseInt(id);
+                Comments comments = commentDao.getCommentsByProductId(productId);
+                HttpSession session = req.getSession(true);
+                session.setAttribute("comments", comments);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         HttpSession session = req.getSession(true);
-        session.setAttribute("product_detail",pro);
-        session.setAttribute("idCate",pro.getItems().get(0).getIdCategory());
+        session.setAttribute("product_detail", pro);
+        session.setAttribute("idCate", pro.getItems().get(0).getIdCategory());
         resp.sendRedirect("getProductSimilar");
-
-
-
     }
 }
