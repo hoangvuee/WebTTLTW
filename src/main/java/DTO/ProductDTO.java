662385d0374fd1e07908757b94d9ff65b1d9805a
NGUYEN BUI HOANG VU <22130328@st.hcmuaf.edu.vn>
2025-06-03 20:53:50 +0700

    thêm lớp DTO để lấy dữ liệu call API đơn hàng

--- src/main/java/DTO/ProductDTO.java
+++ src/main/java/DTO/ProductDTO.java
@@ -1,6 +1,30 @@
 package DTO;
 
+import Models.UserResigter.ListRegister;
+
+import java.util.List;
+
 public class ProductDTO {
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", inventory=" + inventory +
+                ", sale=" + sale +
+                ", status='" + status + '\'' +
+                ", quantity=" + quantity +
+                ", isActive='" + isActive + '\'' +
+                ", idProduct='" + idProduct + '\'' +
+                ", saleDTOS=" + saleDTO +
+                ", importDate='" + importDate + '\'' +
+                '}';
+    }
+
     private String id;
     private String name;
     private String image;
@@ -13,6 +37,68 @@ public class ProductDTO {
     private int quantity;
     private String isActive;
     private String idProduct;
+
+    public SaleDTO getSaleDTO() {
+        return saleDTO;
+    }
+
+    public void setSaleDTO(SaleDTO saleDTO) {
+        this.saleDTO = saleDTO;
+    }
+
+    private SaleDTO saleDTO;
+    private List<PromotionDTO> promotionDTOS;
+    private List<DescriptionDTO> descriptionDTOS;
+
+    public List<DescriptionDTO> getDescriptionDTOS() {
+        return descriptionDTOS;
+    }
+
+    public void setDescriptionDTOS(List<DescriptionDTO> descriptionDTOS) {
+        this.descriptionDTOS = descriptionDTOS;
+    }
+
+    public List<PromotionDTO> getPromotionDTOS() {
+        return promotionDTOS;
+    }
+
+    public void setPromotionDTOS(List<PromotionDTO> promotionDTOS) {
+        this.promotionDTOS = promotionDTOS;
+    }
+
+    public ProductDTO(String id, String name, String image, String category, double price, int weight, int inventory, double sale, String status, int quantity, String isActive, String idProduct, SaleDTO saleDTO, List<PromotionDTO> promotionDTOS, String importDate) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.category = category;
+        this.price = price;
+        this.weight = weight;
+        this.inventory = inventory;
+        this.sale = sale;
+        this.status = status;
+        this.quantity = quantity;
+        this.isActive = isActive;
+        this.idProduct = idProduct;
+        this.saleDTO = saleDTO;
+        this.promotionDTOS = promotionDTOS;
+        this.importDate = importDate;
+    }
+
+    public ProductDTO(String id, String name, String image, String category, double price, int weight, int inventory, String status, int quantity, String isActive, String idProduct, SaleDTO saleDTO, String importDate) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.category = category;
+        this.price = price;
+        this.weight = weight;
+        this.inventory = inventory;
+        this.status = status;
+        this.quantity = quantity;
+        this.isActive = isActive;
+        this.idProduct = idProduct;
+        this.saleDTO = saleDTO;
+        this.importDate = importDate;
+    }
 
     public String getIdProduct() {
         return idProduct;
@@ -137,22 +223,4 @@ public class ProductDTO {
         this.status = status;
     }
 
-    @Override
-    public String toString() {
-        return "ProductDTO{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", image='" + image + '\'' +
-                ", category='" + category + '\'' +
-                ", price=" + price +
-                ", weight=" + weight +
-                ", inventory=" + inventory +
-                ", sale=" + sale +
-                ", status='" + status + '\'' +
-                ", quantity=" + quantity +
-                ", isActive='" + isActive + '\'' +
-                ", idProduct='" + idProduct + '\'' +
-                ", importDate='" + importDate + '\'' +
-                '}';
-    }
 }
